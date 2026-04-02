e0e7b476ea4ed6d98ae4e1101c7c643ba9eabbd8
Azlan21 <azlanqamar51@gmail.com>
2026-04-02 14:29:31 -0600

    DatabaseConnectionException done

---
+++ src/edu/ucalgary/oop/DatabaseConnectionException.java
@@ -0,0 +1,9 @@
+package edu.ucalgary.oop;
+
+public class DatabaseConnectionException extends Exception {
+
+    public DatabaseConnectionException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/edu/ucalgary/oop/DatabaseConnectionManager.java
@@ -0,0 +1,7 @@
+package edu.ucalgary.oop;
+
+public class DatabaseConnectionManager {
+    private static DatabaseConnectionManager instance = null;
+    private Connection connection;
+
+}
